c9817d2fbd60d0bc6b603060b7cfe3e18735ffcf
rafaelreisramos <rrramos@gmail.com>
2024-04-03 15:36:13 -0300

    feat: add cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,6 +6,7 @@ import {
 } from 'fastify-type-provider-zod'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastifyCors } from '@fastify/cors'
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
 import { getEvent } from './routes/get-event'
@@ -18,6 +19,7 @@ export const app = fastify()
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 app.setErrorHandler(errorHandler)
+app.register(fastifyCors, { origin: '*' })
 app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
