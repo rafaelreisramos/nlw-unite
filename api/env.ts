0907c5169574fbf3028900176bc4cd624bba1fd6
rafaelreisramos <rrramos@gmail.com>
2024-04-09 17:11:54 -0300

    chore: change sqlite -> postgresql + docker

--- api/env.ts
+++ api/env.ts
@@ -2,7 +2,10 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   SERVER_PORT: z.string().transform(Number),
-  DATABASE_URL: z.string().url(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  DATABASE_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
